130473fd596a1e75e74825b51c08f14f7f2a15eb
manmacfer <manuelmachuca94@gmail.com>
2022-03-28 05:12:56 +0200

    trabajo terminado

---
+++ js/javascript.js
@@ -0,0 +1,27 @@
+function passw() {
+
+    passw1 = document.formulario.passw1.value
+    passw2 = document.formulario.passw2.value
+
+    if (passw1 == passw2)
+
+       alert("Las contraseñas conciden.")
+
+        else
+
+            alert("Error, las contraseñas no coinciden.")
+}
+
+function email0() {
+
+    email1 = document.formulario.email1.value
+    email2 = document.formulario.email2.value
+
+    if (email1 == email2)
+
+       alert("Los emails conciden.")
+
+        else
+
+            alert("Error, los emails no coinciden.")
+}
